e72af6da5af579baf62d17de27c8abc4badaf4b6
https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/UCSDSDG201907DATA2.git <ruddy.simonpour@gmail.com>
2020-07-31 12:34:13 -0700

    data cleaning added plus analysis using window function

--- Data_cleaning.sql
+++ Data_cleaning.sql
@@ -73,6 +73,91 @@ FROM t1;
 
 
 
+-- You may have noticed that in the previous solution some of the company names include spaces, which will certainly not work in an email address. 
+-- See if you can create an email address that will work by removing all of the spaces in the account name. 
+select primary_poc, concat(left(primary_poc, strpos(primary_poc,' ')-1), '.', right(primary_poc, length(primary_poc) - strpos(primary_poc,' ')), '@', replace(name,' ',''), '.com') as "email_address"
+from accounts
 
+
+
+-- We would also like to create an initial password, which they will change after their first log in. The first password will be the first letter of the primary_poc's first name (lowercase), 
+-- then the last letter of their first name (lowercase), the first letter of their last name (lowercase), the last letter of their last name (lowercase), 
+-- the number of letters in their first name, the number of letters in their last name, and then the name of the company they are working with, all capitalized with no spaces.
+with t1 as(
+select name,
+	   primary_poc,
+	   left(primary_poc, strpos(primary_poc, ' ')-1) as "first_name",
+	   right(primary_poc, length(primary_poc) - strpos(primary_poc, ' ')) as "last_name"
+from accounts
+)
+select concat(lower(right(first_name, 1)), lower(left(first_name,1)), lower(right(last_name,1)), lower(left(last_name,1)), length(first_name), length(last_name), upper(replace(name,' ',''))) as "Password"
+from t1
+
+
+
+
+-- Run the query below to see the missing values
+SELECT COALESCE(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, 
+	   COALESCE(o.account_id,a.id) account_id, o.occurred_at, o.standard_qty, o.gloss_qty, o.poster_qty, o.total, o.standard_amt_usd, o.gloss_amt_usd, o.poster_amt_usd, o.total_amt_usd
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id
+WHERE o.total IS NULL;
+
+
+select *
+from accounts as a
+left join orders as o 
+on a.id = o.account_id
+where o.total is null;
+-- The query above makes a "left join" of tables - accounts and orders, based on join condition a.id = o.account_id. The result of the join will necessarily contain all rows of the accounts table, even if there is no matching row in the orders table.
+
+-- There is a row in the accounts table with the id = 1731 and name = 'Goldman Sachs Group' that does not have a matching row in the orders table. Therefore, the query above will return a row having NULL in each column from the orders table.
+
+-- You will notice that the id column is also blank. One might think that the null value is due to the ambiguous id column as it is present in both the tables. But, even if we use a fully qualified column name a.id, it will not give us the 1731 value.
+
+
+-- Using coalesce to fill in the account.id column with account.id for the null value.
+select coalesce(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, o.*
+from accounts a
+left join orders o
+on a.id = o.account_id
+where o.total is null
+
+
+
+-- Using coalesce to fill in the orders.id column with account.id for the null value.
+select coalesce(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, 
+	   coalesce(o.account_id, a.id) account_id, o.occurred_at, o.standard_qty, o.gloss_qty, o.poster_qty, o.total, o.standard_amt_usd, o.gloss_amt_usd, o.poster_amt_usd, o.total_amt_usd
+from accounts a
+left join orders o
+on a.id = o.account_id
+where o.total is NULL;
+
+
+-- Using coalesce to fill in the qty and usd column with 0.
+select coalesce(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, 
+	   coalesce(o.account_id, a.id) account_id, o.occurred_at, coalesce(o.standard_qty,0) as "standard_qty", coalesce(o.gloss_qty,0) as "gloss_qty", 
+	   coalesce(o.poster_qty,0) "poster_qty", coalesce(o.total,0) as "total", coalesce(o.standard_amt_usd,0) as "standard_amt_usd",
+	   coalesce(o.gloss_amt_usd,0) as "gloss_amt_usd", coalesce(o.poster_amt_usd,0) as "poster_amt_usd", coalesce(o.total_amt_usd,0) as "total_amt_usd"
+from accounts a
+left join orders o
+on a.id = o.account_id
+where o.total is NULL;
+
+
+SELECT COUNT(*)
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id;
+
+
+select coalesce(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, 
+	   coalesce(o.account_id, a.id) account_id, o.occurred_at, coalesce(o.standard_qty,0) as "standard_qty", coalesce(o.gloss_qty,0) as "gloss_qty", 
+	   coalesce(o.poster_qty,0) "poster_qty", coalesce(o.total,0) as "total", coalesce(o.standard_amt_usd,0) as "standard_amt_usd",
+	   coalesce(o.gloss_amt_usd,0) as "gloss_amt_usd", coalesce(o.poster_amt_usd,0) as "poster_amt_usd", coalesce(o.total_amt_usd,0) as "total_amt_usd"
+from accounts a
+left join orders o
+on a.id = o.account_id
 
 
